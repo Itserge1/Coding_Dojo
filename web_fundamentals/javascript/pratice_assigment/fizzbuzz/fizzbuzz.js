458e301d424da96452361d5263fa6b37bb5e8d09
itserge1 <realitserge@gmail.com>
2021-10-17 13:42:19 -0400

    initial commit

---
+++ web_fundamentals/javascript/pratice_assigment/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+x = 0
+while (x <= 100){
+    if (x%3 == 0 && x%5 == 0){
+        console.log("FizzBuzz")
+        x++;
+    }
+    else if(x%3 == 0){
+        console.log("Fizz");
+        x++;
+    }
+    else if(x%5 == 0){
+        console.log("Buzz")
+        x++;
+    }
+    else{
+        console.log(x)
+        x++;
+    }
+}
