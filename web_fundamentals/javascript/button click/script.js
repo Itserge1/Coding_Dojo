c8f444b08ce8faa4de8364608eceff3290d45aba
itserge1 <realitserge@gmail.com>
2021-10-18 00:18:28 -0400

    initial commit

---
+++ web_fundamentals/javascript/button click/script.js
@@ -0,0 +1,10 @@
+function turnOff(element) {
+    element.innerText = "Logout";
+}
+function hide(element) {
+    element.remove();
+}
+function like(element) {
+    console.log("Ninja was liked", element);
+}
+
