ca854946a32f370cc4261c7e71358f2a9cfa9ec4
itserge1 <realitserge@gmail.com>
2021-10-19 21:32:57 -0400

    initial comit

---
+++ web_fundamentals/javascript/Likes/script.js
@@ -0,0 +1,10 @@
+function add(element){
+    document.querySelector(".number").innerText++
+}
+
+function add2(element){
+    document.querySelector(".number1").innerText++
+}
+function add3(element){
+    document.querySelector(".number2").innerText++
+}
