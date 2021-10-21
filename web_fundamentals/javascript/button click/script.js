8d1277d40cff8ffe471fa9339871ef40e1e11ed1
itserge1 <realitserge@gmail.com>
2021-10-21 13:21:22 -0400

    initial comit

--- web_fundamentals/javascript/button click/script.js
+++ web_fundamentals/javascript/button click/script.js
@@ -5,6 +5,12 @@ function hide(element) {
     element.remove();
 }
 function like(element) {
+    document.querySelector("#num").innerHTML++
     alert("Ninja was liked");
 }
+function like2(element) {
+    document.querySelector("#num2").innerHTML++
+    alert("Ninja was liked")
+}
+
 
