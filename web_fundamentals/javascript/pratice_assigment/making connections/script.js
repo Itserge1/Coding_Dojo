efd44ef038dffebc4b6f64bedbc8555aa873bc84
itserge1 <realitserge@gmail.com>
2021-10-22 19:59:16 -0400

    Initial Commit

---
+++ web_fundamentals/javascript/pratice_assigment/making connections/script.js
@@ -0,0 +1,8 @@
+function change(element){
+    document.querySelector("#names1").innerHTML = "Phil K";
+}
+function hide(element){
+    document.querySelector(".todd").remove()
+    document.querySelector("#requestnum").innerHTML--
+    document.querySelector("#connectionnum").innerHTML++
+}
