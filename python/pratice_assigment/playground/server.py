81880532def092f0374f24f926c3e1a23c460b1e
itserge1 <realitserge@gmail.com>
2021-10-30 15:08:56 -0400

    Add a color features

--- python/pratice_assigment/playground/server.py
+++ python/pratice_assigment/playground/server.py
@@ -3,20 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", num=3 , color= "green")
 
 
 @app.route('/play')
 def play():
-    return render_template("index.html")
+    return render_template("index.html", num=3 , color= "green")
 
 @app.route('/play/<int:num>')
 def custum_play(num):
-    return render_template("playground.html",num= num)
+    return render_template("index.html",num= num, color= "green")
 
 @app.route('/play/<int:num>/<string:color>')
 def color_custum_play(num, color):
-    return render_template("playground.html",num= num,color = color)
+    return render_template("index.html",num= num,color = color)
 
 
 
