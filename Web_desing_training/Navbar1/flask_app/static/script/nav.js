d00c62ab6e6f8d5286617d3c79587466503ec297
Itserge1 <realitserge@gmail.com>
2021-11-26 15:46:14 -0500

    Add Responsive Footer

--- Web_desing_training/Navbar1/flask_app/static/script/nav.js
+++ Web_desing_training/Navbar1/flask_app/static/script/nav.js
@@ -1,17 +1,27 @@
+const navMenu = document.getElementById('nav-links')
+const openMenu = document.getElementById('toggle-menu')
+const closeMenu = document.getElementById('close-menu')
 
-var marker = document.querySelector('#marker');
-var item = document.querySelectorAll('nav a');
-
-function indicator(e){
-    marker.style.left = e.offsetLeft +"px";
-    marker.style.width = e.offsetWidth+"px";
-}
-
-item.forEach(link => {
-    link.addEventListener('click', (e)=>{
-        indicator(e.target);
-    })
+console.log(navMenu)
+openMenu.addEventListener('click', () =>{
+    navMenu.classList.toggle('open')
 })
+closeMenu.addEventListener('click', () =>{
+    navMenu.classList.toggle('open')
+})
+// var marker = document.querySelector('#marker');
+// var item = document.querySelectorAll('nav a');
+
+// function indicator(e){
+//     marker.style.left = e.offsetLeft +"px";
+//     marker.style.width = e.offsetWidth+"px";
+// }
+
+// item.forEach(link => {
+//     link.addEventListener('click', (e)=>{
+//         indicator(e.target);
+//     })
+// })
 
 //For html  
 
