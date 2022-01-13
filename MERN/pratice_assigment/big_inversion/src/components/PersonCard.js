eac68cb860e05bc37a932e1123f6e6641879fb33
Itserge1 <realitserge@gmail.com>
2022-01-12 19:05:40 -0500

    initial commit

--- MERN/pratice_assigment/big_inversion/src/components/PersonCard.js
+++ MERN/pratice_assigment/big_inversion/src/components/PersonCard.js
@@ -1,16 +1,14 @@
-import React, { Component} from "react";
+import React from "react";
 
-class PersonCard extends Component {
-    render() {
-        const {firstName, lastName, age, hairColor} = this.props;
-        return (
-            <div>
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: <span id = "age" > {age} </span></p>
-                <p>Hair Color: {hairColor}</p>
-            </div>
-        );
-    }
+
+const PersonCard = props => {
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <p>Age: <span id="age" > {props.age} </span></p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    );
 }
 
 export default PersonCard;
