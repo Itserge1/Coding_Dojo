bfc8213469bc1ac2e1338c97a7f099493257c2c1
Itserge1 <realitserge@gmail.com>
2022-01-24 21:07:55 -0500

    update user class

--- MERN/core_assigment/fake_api/components/User.js
+++ MERN/core_assigment/fake_api/components/User.js
@@ -2,9 +2,8 @@ const faker  = require('@faker-js/faker');
 
 class User {
     constructor() {
-        this.name = faker.commerce.product();
-        this.price = `$${ faker.commerce.price() }`;
-        this.department = faker.commerce.department();
+        this.name = faker.name.firstName();
+        this.address = {street: faker.address.streetName(), city: faker.address.city(), state: faker.address.state(), zipCode: faker.address.zipCode(), country: faker.address.country()};
     }
 }
 // console.log(new User());
